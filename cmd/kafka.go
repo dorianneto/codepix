3e556ef8c293ac0cab90655eeafb1dded3e374c6
Dorian <dorian@heyorca.com>
2022-04-12 13:13:09 -0230

    feat: add kafka producer

---
+++ cmd/kafka.go
@@ -0,0 +1,38 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
+	"github.com/dorianneto/codepix/application/kafka"
+	"github.com/spf13/cobra"
+)
+
+// kafkaCmd represents the kafka command
+var kafkaCmd = &cobra.Command{
+	Use:   "kafka",
+	Short: "Start consuming transactions using Apache Kafka",
+	Run: func(cmd *cobra.Command, args []string) {
+		deliveryChan := make(chan ckafka.Event)
+		producer := kafka.NewKafkaProducer()
+
+		kafka.Publish("Olá Kafka", "test", producer, deliveryChan)
+		kafka.DeliveryReport(deliveryChan)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(kafkaCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// kafkaCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// kafkaCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
--- go.mod
+++ go.mod
@@ -4,10 +4,12 @@ go 1.18
 
 require (
 	github.com/asaskevich/govalidator v0.0.0-20210307081110-f21760c49a8d
+	github.com/confluentinc/confluent-kafka-go v1.8.2
 	github.com/jinzhu/gorm v1.9.16
 	github.com/joho/godotenv v1.4.0
 	github.com/lib/pq v1.10.4
 	github.com/satori/go.uuid v1.2.0
+	github.com/spf13/cobra v1.4.0
 	google.golang.org/grpc v1.45.0
 	google.golang.org/protobuf v1.28.0
 	gorm.io/driver/sqlite v1.3.1
@@ -20,7 +22,6 @@ require (
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.4 // indirect
 	github.com/mattn/go-sqlite3 v1.14.9 // indirect
-	github.com/spf13/cobra v1.4.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	golang.org/x/crypto v0.0.0-20211108221036-ceb1ce70b4fa // indirect
 	golang.org/x/net v0.0.0-20210813160813-60bc85c4be6d // indirect
